80e3101cf913e97ffc5985b9909997831e365095
JoaoMazzoni <joao.mazzoni@5by5.com>
2024-04-22 12:32:29 -0300

    Add project files.

---
+++ VetoresNumeros/Program.cs
@@ -0,0 +1,16 @@
+﻿int numero;
+int[] numeros = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write("Digite o valor do {0}° número: ", i+1);
+    numero = int.Parse(Console.ReadLine());
+    numeros[i] = numero;
+}
+
+Console.WriteLine("\n");
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("O {0}° número digitado é: {1}", i+1, numeros[i]);
+}
